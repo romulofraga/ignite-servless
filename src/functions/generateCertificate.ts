317852490711e7cf93c54cd1015a7f38c88d85a9
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2021-06-12 19:38:42 -0300

    Salvando os arquivos em PDF no S3

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -3,6 +3,7 @@ import path from "path";
 import fs from "fs";
 import handlebars from "handlebars";
 import dayjs from 'dayjs';
+import {S3} from 'aws-sdk';
 
 import { document } from '../utils/dynamodbClient';
 
@@ -68,17 +69,29 @@ export const handle = async (event) => {
   const contentPDF = await page.pdf({
     format: "a4",
     landscape: true,
-    path: process.env.IS_OFFLINE ? "certificate.pdf": null,
+    path: process.env.IS_OFFLINE ? `${Date.now()}certificate.pdf`: null,
     printBackground: true,
     preferCSSPageSize: true
-  })
+  });
 
    await (await browser).close()
+
+   const s3 = new S3();
+
+    await s3.putObject({
+      Bucket: "servlesscreatecertificate",
+      Key: `${id}.pdf`,
+      ACL: "public-read",
+      Body: contentPDF,
+      ContentType: "application/pdf"
+    }).promise()
+
 
   return {
     statusCode: 201,
     body: JSON.stringify({
-      message: "Certificate created!"
+      message: "Certificated created",
+      URL: `https://servlesscreatecertificate.s3.sa-east-1.amazonaws.com/${id}.pdf`
     }),
     headers: {
       "Content-Type":"application/json"
